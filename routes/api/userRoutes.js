aef2bc1b3708644e70b8485ef09c49c24a7f38c6
Cheryl L <cheryl.loeffler@gmail.com>
2023-06-24 12:41:21 -0600

    Set Up
    - Created directories and files
    - Created connections between directories
    - Outlined/psuedocoded known portions of app
    - Created seed data

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+
+//Import CRUD operations from controller
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+    .route('./:thoughtId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
+
+
+// GET all users
+
+// GET a single user by its _id and populated thought and friend data
+
+// POST a new user:
+
+// // example data
+// {
+//   "username": "lernantino",
+//   "email": "lernantino@gmail.com"
+// }
+// PUT to update a user by its _id
+
+// DELETE to remove user by its _id
+
+// BONUS: Remove a user's associated thoughts when deleted.
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
---
+++ utils/data.js
@@ -0,0 +1,65 @@
+// Array of usernames
+const usernames = [
+    "john.doe",
+    "emma.watson",
+    "alexsmith",
+    "lilywhite",
+    "mike_johnson",
+    "sarahgreen",
+    "david.brown",
+    "olivia_wilson",
+    "andrew88",
+    "jennifer_parker",
+    "samuel.adams",
+    "emily.jones",
+    "matthew.baker",
+    "isabella_smith",
+    "nathan_davis",
+    "chloe.thompson",
+    "robertmiller",
+    "amelia.johnson",
+    "william.gray",
+    "sophia_taylor"
+  ];
+  
+  // Array of emails
+  const emails = [
+    "john.doe@example.com",
+    "emma.watson@example.com",
+    "alexsmith@example.com",
+    "lilywhite@example.com",
+    "mike_johnson@example.com",
+    "sarahgreen@example.com",
+    "david.brown@example.com",
+    "olivia_wilson@example.com",
+    "andrew88@example.com",
+    "jennifer_parker@example.com",
+    "samuel.adams@example.com",
+    "emily.jones@example.com",
+    "matthew.baker@example.com",
+    "isabella_smith@example.com",
+    "nathan_davis@example.com",
+    "chloe.thompson@example.com",
+    "robertmiller@example.com",
+    "amelia.johnson@example.com",
+    "william.gray@example.com",
+    "sophia_taylor@example.com"
+  ];
+  
+
+const friends = [
+    //actually will probably write function so some number of friends are pulled from the friends list instead of making an array. 
+];
+
+const getRandomUsername = () => {
+
+}
+
+const getRandomFriends = () => {
+    
+};
+
+module.exports = {
+    getRandomUsername,
+    getRandomFriends
+}
